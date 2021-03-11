8950182c2dbb8153e3d06943c7822127658857f2
Matteo Bauce <mbauce@cern.ch>
2021-03-11 17:47:18 +0100

    restarting

---
+++ analysis/plotAcceptances.py
@@ -0,0 +1,54 @@
+import argparse
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+parser = argparse.ArgumentParser(description="mUA9 parser")
+parser.add_argument('--input', action="store", dest="fileName", default="Acceptances.txt", help='input file name')
+args = parser.parse_args()
+
+#settings
+plt.style.use('fast')
+colNames=['angle','eff1S','eff2S','label']
+bendAngle=61.0
+dataLabel='ACP80'
+mcLabel='Sim'
+yMin=0.0
+yMax=1.0
+dataset = pd.read_csv(args.fileName,sep=" ",comment='#',names=colNames)
+print(dataset.label)
+
+dsData = dataset[dataset['label'].str.contains(dataLabel)]
+print(dsData)
+
+dsMC = dataset[dataset['label'].str.contains(mcLabel)]
+print(dsMC)
+
+fig = plt.figure(figsize=(6,4))
+
+plt.plot(dsMC['angle'],dsMC['eff1S'],'--r',label='$A_{1\sigma}$ Simulation')
+plt.plot(dsMC['angle'],dsMC['eff2S'],'--b',label='$A_{2\sigma}$ Simulation')
+#
+plt.plot(dsData['angle'],dsData['eff1S'],'or',label='$A_{1\sigma}$ Data (%s)'%dataLabel)
+plt.plot(dsData['angle'],dsData['eff2S'],'ob',label='$A_{2\sigma}$ Data (%s)'%dataLabel)
+
+plt.title(r'Acceptance $\theta_b$ +/- 1(2)$\sigma$  ($\theta_b$= {:.1f} $\mu$rad)'.format(bendAngle))
+plt.xlabel('Beam angle',fontsize='15')
+plt.ylabel('Window acceptance',fontsize='15')
+plt.ylim(yMin,yMax)
+plt.grid(True)
+plt.vlines(-1*bendAngle,yMin,yMax)
+#plt.legend(loc="best", bbox_to_anchor=(1,1), fontsize=17, framealpha=0.1)
+plt.legend(loc="upper right", fontsize=12, framealpha=0.1)
+for x,y in zip(dsData['angle'],dsData['eff1S']):
+    label = "{:.2f}".format(y)
+    plt.annotate(label,(x,y),textcoords="offset points",xytext=(0,10),ha='center',color='r')
+for x,y in zip(dsData['angle'],dsData['eff2S']):
+    label = "{:.2f}".format(y)
+    plt.annotate(label,(x,y),textcoords="offset points",xytext=(0,10),ha='center',color='b')
+
+plt.annotate("Alignement",(-1*bendAngle,0.7),textcoords="offset points",xytext=(10,0),ha='center',rotation=90)
+
+
+plt.show()
+fig.savefig("Acceptances.png",bbox_inches='tight')
---
+++ dumpRootToCSV.py
@@ -0,0 +1,38 @@
+import argparse
+
+parser = argparse.ArgumentParser(description="mUA9 parser")
+parser.add_argument('--fileName', action="store", dest="fileName", default="build/mUA9COVID_Part_Ene180_CrystAng0_N1000.root", help='input file name')
+parser.add_argument('--tree', action="store", dest="tree", default="ExpTree", help='list of tree you want to process')
+parser.add_argument('--out', action="store", dest="out", help='list of tree you want to process')
+args = parser.parse_args()
+
+from ROOT import TFile
+import random
+
+inFile = TFile(args.fileName)
+tree = inFile.Get(args.tree)
+if args.out:
+    outName=args.out
+else:
+    outName=args.fileName.replace(".root",".dat")
+
+outFile = open(outName,"w")
+
+#smearing 10.6 microradians
+smearSigmaX = 10.15
+smearSigmaX = 9.0
+smearSigmaX = 0.0
+# Loop over events
+nEntries=tree.GetEntriesFast()
+#print("Start looping over",nEntries,"events. Please wait...")
+for i, e in enumerate(tree): 
+    #if ((i/nEntries*100)%10) == 0 : print(i/nEntries*100,"% done")
+    if len(e.angXin)!=0:
+        #thetaXin=(e.sdHitX[1]-e.sdHitX[0])/(e.sdHitZ[1]-e.sdHitZ[0])/murad
+        #cols=[gonio_angle,d0_x,d0_y,thetaIn_x,thetaOut_x,thetaIn_y,thetaOut_y,unknown1,unknown2]
+        #units= urad, mm, mm, urad, urad, urad, urad
+        smearX = random.gauss(0.0,smearSigmaX)
+        #print(smearX)
+        outFile.write("{:f},{:.16f},{:.16f},{:.15e},{:.15e},{:.15e},{:.15e},{:.1f},{:.1f}\n".format(e.crystAngX[0],e.posXin[0],e.posYin[0],
+                                                                    e.angXin[0]*1E-6,(e.angXout[0]+smearX)*1E-6,e.angYin[0]*1E-6,e.angYout[0]*1E-6,0.0,0.0))
+outFile.close()
